5c4fd06f6a9bb391faf9fb9d7d92808a5ba4cbdd
Romain Giot <giot.romain@gmail.com>
2013-06-10 22:15:58 +0200

    Initial commit

---
+++ image_colors.asm
@@ -0,0 +1,15 @@
+; Color extraction
+; Krusty/Benediction
+	db 0x4c	; ink 0
+	db 0x58	; ink 1
+	db 0x5d	; ink 2
+	db 0x57	; ink 3
+	db 0x5b	; ink 4
+	db 0x54	; ink 5
+	db 0x40	; ink 6
+	db 0x4a	; ink 7
+	db 0x43	; ink 8
+	db 0x4e	; ink 9
+	db 0x4e	; ink 10
+	db 0x5c	; ink 11
+	db 0x4b	; ink 12
